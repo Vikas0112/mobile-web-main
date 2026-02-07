73f53a88f10b81f0af3bb0e2c74873364c2d3a40
Vikas Rajput <vikas0112@users.noreply.github.com>
2026-02-08 00:22:54 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        pinkPrimary: "#e91e63",
+        pinkLight: "#fdecef",
+        pinkSoft: "#fff5f7",
+        pinkBorder: "#f8c6d0"
+      }
+    }
+  },
+  plugins: []
+};
