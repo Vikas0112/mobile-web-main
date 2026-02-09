c63fba6f24adc216129fc715a2e2db507fbf09b4
Vikas Rajput <167027840+Vikas0112@users.noreply.github.com>
2026-02-10 01:33:14 +0530

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 const Dashboard = () => (
   <div className="p-10 bg-white max-w-md mx-auto">
     <h2 className="text-2xl text-pinkPrimary">User Dashboard</h2>
-    <p>Name: John Doe</p>
+    <p>Name: vikas rajput</p>
     <p>Email: john@test.com</p>
   </div>
 );
